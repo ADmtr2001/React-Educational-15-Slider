5659d47bb1596d060a55dbf4a04f9db84f42462a
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-11-12 19:00:26 +0200

    replace useEffect with nextSlide and prevSlide functions

--- src/App.js
+++ src/App.js
@@ -6,15 +6,36 @@ function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    const lastIndex = people.length - 1;
-    if (index < 0) setIndex(lastIndex);
-    if (index > lastIndex) setIndex(0);
-  }, [index, people]);
+  const nextSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex + 1;
+      if (index > people.length - 1) index = 0;
+      return index;
+    });
+  };
+
+  const prevSlide = () => {
+    setIndex((oldIndex) => {
+      let index = oldIndex - 1;
+      if (index < 0) index = people.length - 1;
+
+      return index;
+    });
+  };
+
+  // useEffect(() => {
+  //   const lastIndex = people.length - 1;
+  //   if (index < 0) setIndex(lastIndex);
+  //   if (index > lastIndex) setIndex(0);
+  // }, [index, people]);
 
   useEffect(() => {
     let slider = setInterval(() => {
-      setIndex(index + 1);
+      setIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) index = 0;
+        return index;
+      });
     }, 3000);
     return () => clearInterval(slider);
   }, [index]);
@@ -48,8 +69,8 @@ function App() {
             </article>
           )
         })}
-        <button className='prev' onClick={() => setIndex(index - 1)}><FiChevronLeft /></button>
-        <button className='next' onClick={() => setIndex(index + 1)}><FiChevronRight /></button>
+        <button className='prev' onClick={prevSlide}><FiChevronLeft /></button>
+        <button className='next' onClick={nextSlide}><FiChevronRight /></button>
       </div>
     </section>
   )
